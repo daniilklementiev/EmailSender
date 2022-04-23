b962b593b5a60bb394f4151573ba9d9843d9bea3
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-04-23 09:36:01 +0300

    + english

--- EmailSender/Form1.cs
+++ EmailSender/Form1.cs
@@ -5,28 +5,28 @@ namespace EmailSender
 {
     public partial class Form1 : Form
     {
-        private MailAddress from;           // отправитель
-        private MailAddress to;             // получатель
-        private MailMessage mailMessage;    // сообщение
-        private SmtpClient smtp;            // клиент для отправки сообщений
-        
+        private MailAddress from;           // отправитель - sender
+        private MailAddress to;             // получатель - recipient
+        private MailMessage mailMessage;    // сообщение - message
+        private SmtpClient smtp;            // клиент для отправки сообщений - client
+
         public Form1()
         {
-            from = new MailAddress("testprogramcsharp@gmail.com", "Daniil"); // отправитель
-            to = new MailAddress("danik1708@ukr.net"); // получатель
-            mailMessage = new MailMessage(from, to); // создаем сообщение
-            mailMessage.Subject = "Test"; // тема сообщения
-            mailMessage.Body = "<h2>Письмо-тест работы smtp-клиента</h2>"; // тело сообщения
-            mailMessage.IsBodyHtml = true; // письмо в виде html
-            smtp = new SmtpClient("smtp.gmail.com", 587); // создаем клиента для отправки сообщений
-            smtp.Credentials = new NetworkCredential("testprogramcsharp@gmail.com", "123456"); // присваиваем логин и пароль
-            smtp.EnableSsl = true; // включаем протокол ssl
+            from = new MailAddress("testprogramcsharp@gmail.com", "Daniil"); // отправитель - sender
+            to = new MailAddress("danik1708@ukr.net"); // получатель - recipient
+            mailMessage = new MailMessage(from, to); // создаем сообщение - creating message
+            mailMessage.Subject = "Test"; // тема сообщения - subject
+            mailMessage.Body = "<h2>Test mail</h2>"; // тело сообщения - body
+            mailMessage.IsBodyHtml = true; // письмо в виде html - html mail
+            smtp = new SmtpClient("smtp.gmail.com", 587); // создаем клиента для отправки сообщений - creating smtp client
+            smtp.Credentials = new NetworkCredential("testprogramcsharp@gmail.com", "123456"); // присваиваем логин и пароль - assigning login and password
+            smtp.EnableSsl = true; // включаем протокол ssl - enabling ssl protocol
             InitializeComponent();
         }
 
         private void buttonSend_Click(object sender, EventArgs e)
         {
-            smtp.Send(mailMessage); // отправляем сообщение
+            smtp.Send(mailMessage); // отправляем сообщение - sending message
         }
     }
 }
