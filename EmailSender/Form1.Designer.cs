8ce4402888baf3303735989c6595e4e7d6e2e66c
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-04-23 09:32:09 +0300

    Add project files.

---
+++ EmailSender/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace EmailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To:";
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(133, 44);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(200, 39);
+            this.textBoxFrom.TabIndex = 2;
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(133, 96);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(200, 39);
+            this.textBoxTo.TabIndex = 2;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(153, 154);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(150, 46);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxFrom;
+        private TextBox textBoxTo;
+        private Button buttonSend;
+    }
+}
---
+++ EmailSender/Form1.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender
+{
+    public partial class Form1 : Form
+    {
+        private MailAddress from;           // отправитель
+        private MailAddress to;             // получатель
+        private MailMessage mailMessage;    // сообщение
+        private SmtpClient smtp;            // клиент для отправки сообщений
+        public Form1()
+        {
+            from = new MailAddress("testprogramcsharp@gmail.com", "Daniil"); // отправитель
+            to = new MailAddress("danik1708@ukr.net"); // получатель
+            mailMessage = new MailMessage(from, to); // создаем сообщение
+            mailMessage.Subject = "Test"; // тема сообщения
+            mailMessage.Body = "<h2>Письмо-тест работы smtp-клиента</h2>"; // тело сообщения
+            mailMessage.IsBodyHtml = true; // письмо в виде html
+            smtp = new SmtpClient("smtp.gmail.com", 587); // создаем клиента для отправки сообщений
+            smtp.Credentials = new NetworkCredential("testprogramcsharp@gmail.com", "232345232345"); // присваиваем логин и пароль
+            smtp.EnableSsl = true; // включаем протокол ssl
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            smtp.Send(mailMessage); // отправляем сообщение
+        }
+    }
+}
